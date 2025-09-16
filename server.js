a3e3bba161f79a67ea4026d92446649c3d7eccd0
Poojan P Shrivastav <poojanshrivastav21@gmail.com>
2025-09-16 19:35:18 +0530

    project done. files auto-create on server start.

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const http = require('http')
 const PORT = 5001;
 const fs = require('fs')
 const path = require('path')
+
+// Ensure src directory and HTML files exist
+const srcDir = path.join(__dirname, 'src');
+if (!fs.existsSync(srcDir)) {
+  fs.mkdirSync(srcDir);
+}
+const indexPath = path.join(srcDir, 'index.html');
+const aboutPath = path.join(srcDir, 'about.html');
+if (!fs.existsSync(indexPath)) {
+  fs.writeFileSync(indexPath, '<h1>Home Page</h1>');
+}
+if (!fs.existsSync(aboutPath)) {
+  fs.writeFileSync(aboutPath, '<h1>About Page</h1>');
+}
+
 const app = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
-      const homePath = path.join(__dirname, 'src/index.html')
-      fs.readFile(homePath, 'utf-8', (err, content) => {
+      fs.readFile(indexPath, 'utf-8', (err, content) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/html' });
           res.end('<h1>Error loading home page</h1>');
@@ -17,7 +31,6 @@ const app = http.createServer((req, res) => {
       })
       break;
     case '/about':
-      const aboutPath = path.join(__dirname, 'src/about.html')
       fs.readFile(aboutPath, 'utf-8', (err, content) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/html' });
