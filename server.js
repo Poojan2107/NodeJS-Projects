58a37e759d012c47771c782ffa50017db335b2b5
Poojan P Shrivastav <poojanshrivastav21@gmail.com>
2025-09-16 19:32:43 +0530

    project done.

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-// Custom HTTP server using Node.js core 'http' module
-const http = require('http');
+const http = require('http')
+const PORT = 5001;
+const fs = require('fs')
+const path = require('path')
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      const homePath = path.join(__dirname, 'src/index.html')
+      fs.readFile(homePath, 'utf-8', (err, content) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/html' });
+          res.end('<h1>Error loading home page</h1>');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content)
+        }
+      })
+      break;
+    case '/about':
+      const aboutPath = path.join(__dirname, 'src/about.html')
+      fs.readFile(aboutPath, 'utf-8', (err, content) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/html' });
+          res.end('<h1>Error loading about page</h1>');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content)
+        }
+      })
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end("<h1>404 page not found</h1>")
+  }
+})
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from custom Node.js HTTP server!');
-});
-
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
